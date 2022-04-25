088d221d694c800f90053f9e84a3fec32e09ca1e
sandeepvalekar <104376879+sandeepvalekar@users.noreply.github.com>
2022-04-25 21:12:17 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,21 @@
+const mysql=require('mysql');
+
+var mysqlConnection=mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"password",
+  database:"Crud",
+  multipleStatements:true
+
+});
+mysqlConnection.connect((err)=>{
+if(!err)
+{
+  console.log("Connected");
+}
+else{
+  console.log("Connection Failed");
+}
+});
+
+module.exports=mysqlConnection;
